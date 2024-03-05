4e84468fa438e434944394e7d653dc8f53f44b9d
Yargachin-wye <78979106+Yargachin-wye@users.noreply.github.com>
2024-03-06 02:47:19 +0700

    installed ML agents

--- Assets/С# Scripts/Agent/TestML.cs
+++ Assets/С# Scripts/Agent/TestML.cs
@@ -5,45 +5,48 @@ using Unity.MLAgents;
 using Unity.MLAgents.Actuators;
 using Unity.MLAgents.Sensors;
 using Unity.Barracuda;
+using Unity.VisualScripting;
 
 public class TestML : Agent
 {
+    [SerializeField] private float TimeScale;
     [SerializeField] private Transform spawnsAgent;
     [SerializeField] private Transform spawnsTarget;
     [SerializeField] private Transform target;
     [SerializeField] private float wallReward = -1;
     [SerializeField] private float winReward = +1;
-    /*
-    [SerializeField] private PhysicsMaterial2D winMaterial;
-    [SerializeField] private PhysicsMaterial2D loMaterial;
-    */
-
     [SerializeField] private float spead;
+    private void Awake()
+    {
+        Time.timeScale = TimeScale;
+    }
     public override void OnEpisodeBegin()
     {
-        base.OnEpisodeBegin();
-        transform.position = spawnsAgent.GetChild(Random.RandomRange(0, spawnsAgent.childCount)).position;
-        target.position = spawnsTarget.GetChild(Random.RandomRange(0, spawnsTarget.childCount)).position;
+        transform.position = spawnsAgent.GetChild(Random.Range(0, spawnsAgent.childCount)).position;
+        target.position = spawnsTarget.GetChild(Random.Range(0, spawnsTarget.childCount)).position;
     }
 
     public override void CollectObservations(VectorSensor sensors)
     {
-        sensors.AddObservation(new Vector2(transform.position.x, transform.position.y));
-        sensors.AddObservation(new Vector2(target.position.x, target.position.y));
+        sensors.AddObservation(new Vector3(transform.position.x, transform.position.y, 0));
+        sensors.AddObservation(new Vector3(target.position.x, target.position.y, 0));
     }
     public override void OnActionReceived(ActionBuffers actions)
     {
         Vector3 move = new Vector3(actions.ContinuousActions[0], actions.ContinuousActions[1], 0);
-        transform.position += move.normalized * spead;
+        transform.position += move.normalized * spead * Time.deltaTime;
+        
     }
-    
+
     public override void Heuristic(in ActionBuffers actionsOut)
     {
+        base.Heuristic(actionsOut);
         ActionSegment<float> continuousActions = actionsOut.ContinuousActions;
+
         continuousActions[0] = Input.GetAxisRaw("Horizontal");
-        continuousActions[0] = Input.GetAxisRaw("Vertical");
+        continuousActions[1] = Input.GetAxisRaw("Vertical");
     }
-    
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.name == target.gameObject.name)
